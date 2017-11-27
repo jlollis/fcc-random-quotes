af3c7eaeb48f8d9270f882c9162d4ecc35e3989a
jlollis <12737017+jlollis@users.noreply.github.com>
2017-11-26 19:30:39 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var quotes = {
+  'There are many ways to skin a cat, as the saying goes, but the truth is that most of them are pretty inhumane.',
+  'It\'s better to ask forgiveness than it is to get permission.',
+  'Well-behaved women seldom make history.',
+}
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('myQuote').innerHTML = quotes[randomNumber];
+}
